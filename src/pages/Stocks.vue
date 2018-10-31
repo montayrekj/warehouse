9b8e2217b11c7d71be59b181ea6f7506d42a3b78
asi.kmontayre <kmontayre@asi-dev5.com>
2018-10-31 19:45:15 +0800

    Added Stocks modal

--- src/pages/Stocks.vue
+++ src/pages/Stocks.vue
@@ -2,6 +2,10 @@
     <div class="row">
       <div class="col-12">
         <card :title="table1.title">
+          <div class="pull-right" style="padding-bottom: 10px">
+            <button class="btn btn-sm btn-success" style="margin-right: 5px" v-on:click="toggleModal('addStock')">Stock +</button>
+            <button class="btn btn-sm btn-danger" v-on:click="toggleModal('removeStock')">Stock -</button>
+          </div>
           <div class="table-responsive">
             <base-table :data="table1.data"
                         :columns="table1.columns"
@@ -10,22 +14,31 @@
           </div>
         </card>
       </div>
+      <sweet-modal ref="addStockModal" hide-close-button overlay-theme="dark" modal-theme="dark" title="Add Stocks">
+        This is an error…
 
-      <div class="col-12">
-        <card class="card-plain">
-          <div class="table-full-width table-responsive">
-            <base-table :title="table2.title" :sub-title="table2.subTitle" :data="table2.data"
-                         :columns="table2.columns">
-
-            </base-table>
+        <button slot="button" class="btn btn-sm btn-success" style="margin-right: 5px">Save</button>
+        <button slot="button" v-on:click="toggleModal('addStock')" class="btn btn-sm btn-danger">Cancel</button>
+      </sweet-modal>
+      <sweet-modal ref="removeStockModal" hide-close-button overlay-theme="dark" modal-theme="dark" title="Remove Stocks">
+        <div class="row">
+          <span class="col-md-6" style="height: 35px;line-height: 35px;">Product Name: &nbsp Sample Product</span>
+    
+          <div class="col-md-5">
+            <div class="row">
+            <span style="height: 35px;line-height: 35px;margin-right: 10px">Quantity: </span>
+            <input type="number" class="form-control col-md-4" id="stockQuantity" style="background-color: #1c2a38">
+            </div>
           </div>
-        </card>
-      </div>
-
+        </div>
+        <button slot="button" class="btn btn-sm btn-success" style="margin-right: 5px">Save</button>
+        <button slot="button" v-on:click="toggleModal('removeStock')" class="btn btn-sm btn-danger">Cancel</button>
+      </sweet-modal>
     </div>
 </template>
 <script>
 import { BaseTable } from "@/components";
+import { SweetModal, SweetModalTab } from 'sweet-modal-vue'
 const tableColumns = ["Name", "Country", "City", "Salary"];
 const tableData = [
   {
@@ -81,21 +94,41 @@ const tableData = [
 
 export default {
   components: {
-    BaseTable
+    BaseTable,
+    SweetModal,
+    SweetModalTab,
   },
   data() {
     return {
       table1: {
-        title: "Simple Table",
+        title: "Stocks",
         columns: [...tableColumns],
-        data: [...tableData]
+        data: [...tableData],
+        selectedId: [1,3,6]
       },
-      table2: {
-        title: "Table on Plain Background",
-        columns: [...tableColumns],
-        data: [...tableData]
-      }
+      modalFlag: false
     };
+  },
+  methods: {
+    toggleModal: function(origin){
+      if(origin === "addStock") {
+        if(!this.$data.modalFlag) {
+          this.$refs.addStockModal.open()
+          this.$data.modalFlag = true;
+        } else {
+          this.$refs.addStockModal.close()
+          this.$data.modalFlag = false;
+        }
+      } else if(origin === "removeStock") {
+        if(!this.$data.modalFlag) {
+          this.$refs.removeStockModal.open()
+          this.$data.modalFlag = true;
+        } else {
+          this.$refs.removeStockModal.close()
+          this.$data.modalFlag = false;
+        }
+      }
+    }
   }
 };
 </script>
